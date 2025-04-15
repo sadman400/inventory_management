65db81a1aa03db5bd1385e8c0f9570b04f6b2120
sadman400 <sadmanhossain400@gmail.com>
2025-04-15 19:27:00 +0600

    add logout

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,4 @@ Route::post('/login', [UserController::class, 'user_login']);
 Route::post('/send-otp', [UserController::class, 'send_otp']);
 Route::post('/verify-otp', [UserController::class, 'verify_otp']);
 Route::post('/reset-password', [UserController::class, 'password_reset'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/logout', [UserController::class, 'logout']);
