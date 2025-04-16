d82a4a7854efb93cb3a9f86a35ce76a4c7a15e68
sadman400 <sadmanhossain400@gmail.com>
2025-04-16 23:10:20 +0600

    add category controller

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\TokenVerificationMiddleware;
 use Illuminate\Support\Facades\Route;
 
 
+// user 
 Route::post('/register', [UserController::class, 'user_register']);
 Route::post('/login', [UserController::class, 'user_login']);
 Route::post('/send-otp', [UserController::class, 'send_otp']);
@@ -13,3 +15,11 @@ Route::post('/reset-password', [UserController::class, 'password_reset'])->middl
 Route::get('/logout', [UserController::class, 'logout']);
 Route::get('/user-profile', [UserController::class, 'user_profile'])->middleware([TokenVerificationMiddleware::class]);
 Route::post('/update-profile', [UserController::class, 'update_profile'])->middleware([TokenVerificationMiddleware::class]);
+
+
+// category 
+Route::post('/create-category', [CategoryController::class, 'create_category']);
+Route::get('/category-list', [CategoryController::class, 'category_list']);
+Route::post('/category-by-id', [CategoryController::class, 'category_by_id']);
+Route::post('/update-category', [CategoryController::class, 'update_category']);
+Route::post('/delete-category', [CategoryController::class, 'delete_category']);
